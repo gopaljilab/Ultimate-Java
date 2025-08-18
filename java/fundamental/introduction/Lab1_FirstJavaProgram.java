201bd491605130750b5b2a1d52a9562f7f5bfe17
Gopal Gupta <gopaljilab@gmail.com>
2025-08-18 15:48:33 +0530

    Lab1

---
+++ java/fundamental/introduction/Lab1_FirstJavaProgram.java
@@ -0,0 +1,8 @@
+package java.fundamental.introduction;
+
+public class Lab1_FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("World to My java University");
+        // Write Your Code Here
+    }
+}
