de3440a94a7b2555a0fd7c1e2cda7d6bf447e56a
Gopal Gupta <gopaljilab@gmail.com>
2025-08-18 22:42:01 +0530

    Lab2 Variables & DataTypes

--- src/fundamental/types/Lab2_Variables_DataTypes.java
+++ src/fundamental/types/Lab2_Variables_DataTypes.java
@@ -6,14 +6,13 @@ public class Lab2_Variables_DataTypes {
         int number2 = number1;           //Assigning the value
         System.out.println(number2);
         System.out.println(number1 + " ");  // with the + operator we achieve the concatenation
-                                            //where we can add numbers and strings together
+        //where we can add numbers and strings together
 
         char single_character = 'S';      // we can store a single Character in char data type
-                                         // and assign the value in '' single quotes
+        // and assign the value in '' single quotes
         System.out.println("Char : " + single_character);
         float float_Value = 0.77F;         // if we don't tell the datatype using casting as F
-                                            //because Java take the 0.77 as int .
-z
+        //because Java take the 0.77 as int .
         System.out.println("float-Value : " + float_Value);
         double decimalValue = 8.9799994903;
         System.out.println("Double-Value : " + decimalValue);
