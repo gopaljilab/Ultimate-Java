585767b26e42bfb2826b06f6de82ca4fb72175f3
Gopal Gupta <gopaljilab@gmail.com>
2025-08-20 15:11:04 +0530

    Lab2-IfElse

--- src/fundamental/controlFlow/Lab2_1IfElseStatement.java
+++ src/fundamental/controlFlow/Lab2_1IfElseStatement.java
@@ -1,7 +1,26 @@
 package src.fundamental.controlFlow;
 
-public class Lab2_1ControlStatement {
+public class Lab2_1IfElseStatement {
     public static void main(String[] args) {
+        int age = 14;
+        if (age < 5){
+            System.out.println("Child");
+        }
+        else if (age >= 5 && age < 18){
+            System.out.println("Teenager");
+        }
+        else if(age>=18 && age < 40){
+            System.out.println("Adult");
+        }else {
+            System.out.println("Old");
+        }
 
+
+        int checkAge = 28;
+
+        System.out.println(checkAge<30 ? "Young" : "Old");    // check the condition if its
+                                                            // true return Young then false
+
+//        switch ()  --- > to execute diff part of code we use the Switch Statement
     }
 }
