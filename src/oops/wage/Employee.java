125a66cc87160886baaee84cf4207a3e6997c830
Gopal Gupta <gopaljilab@gmail.com>
2025-08-22 21:16:55 +0530

    Lab2-Wage

--- src/oops/wage/Employee.java
+++ src/oops/wage/Employee.java
@@ -1,4 +1,35 @@
 package src.oops.wage;
 
-public class CalculateWage {
+public class Employee {
+    private int baseSalary;
+    private int hourlyRate;
+
+    public Employee(int baseSalary, int hourlyRate){          // Custom Constructor
+        setBaseSalary(baseSalary);
+        setHourlyRate(hourlyRate);
+    }
+
+    private int getHourlyRate() {
+        return hourlyRate;
+    }
+
+    private void setHourlyRate(int hourlyRate) {
+        this.hourlyRate = hourlyRate;
+        if (hourlyRate <= 0)
+            throw new IllegalArgumentException("Hourly rate can't be 0 or less 1");
+    }
+
+    private int getBaseSalary() {
+        return baseSalary;
+    }
+
+    private void setBaseSalary(int baseSalary) {
+        this.baseSalary = baseSalary;
+        if (baseSalary <= 0)
+            throw new IllegalArgumentException("Base Salary can't be 0 or less 1");
+    }
+
+    public int calculateWages(int extraHours){
+        return baseSalary + (hourlyRate * extraHours);
+    }
 }
--- src/oops/wage/Main.java
+++ src/oops/wage/Main.java
@@ -1,4 +1,11 @@
 package src.oops.wage;
 
 public class Main {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee(50_000,200);
+//        employee1.setBaseSalary(50_000);          Encapsulation
+//        employee1.setHourlyRate(200);
+        int wages = employee1.calculateWages(20);
+        System.out.println("Total Salary : " + wages);
+    }
 }
