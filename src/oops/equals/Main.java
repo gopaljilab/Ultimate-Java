22f40cf3c73c8d8559a4a5e10955bbc745bb2a81
Gopal Gupta <gopaljilab@gmail.com>
2025-08-23 23:48:30 +0530

    Equals

--- src/oops/equals/Main.java
+++ src/oops/equals/Main.java
@@ -1,4 +1,11 @@
 package src.oops.equals;
 
-public class main {
+public class Main {
+    public static void main(String[] args) {
+        Point point1 = new Point("Hello");
+        Point point2 = new Point("Hello");
+
+        System.out.println(point1 == point2);
+        System.out.println(point1.equals(point2));
+    }
 }
--- src/oops/equals/Point.java
+++ src/oops/equals/Point.java
@@ -1,4 +1,23 @@
 package src.oops.equals;
 
+import java.util.Objects;
+
 public class Point {
+    private String x;
+
+    public Point(String x) {
+        this.x = x;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x);
+    }
 }
