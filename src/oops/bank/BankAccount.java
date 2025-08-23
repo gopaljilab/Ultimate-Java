e7ad425727253bd9b3982e7b9f26e9a86e42a4f8
Gopal Gupta <gopaljilab@gmail.com>
2025-08-23 23:47:20 +0530

    Bank

--- src/oops/bank/BankAccount.java
+++ src/oops/bank/BankAccount.java
@@ -1,4 +1,26 @@
 package src.oops.bank;
 
-public class BankAccount {
+class BankAccount {
+    private String accountHolder;
+    private double balance;
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public void setAccountHolder(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        if (balance >= 0) {
+            this.balance = balance;
+        } else {
+            System.out.println("Balance cannot be negative!");
+        }
+    }
 }
--- src/oops/bank/Main.java
+++ src/oops/bank/Main.java
@@ -1,4 +1,17 @@
 package src.oops.bank;
 
 public class Main {
+    public static void main(String[] args) {
+        BankAccount acc = new BankAccount();
+
+        System.out.println("Welcome to State Bank Of India");
+        System.out.println("--------------------------------");
+        acc.setAccountHolder("Gopal");
+        acc.setBalance(5000);
+
+        System.out.println("Account Holder: " + acc.getAccountHolder());
+        System.out.println("Balance: ₹" + acc.getBalance());
+
+        acc.setBalance(-2000); // Invalid update → will be blocked
+    }
 }
