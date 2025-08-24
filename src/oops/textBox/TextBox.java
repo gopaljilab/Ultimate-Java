fe7e966fc16a3e0db782d7cf285c8bab2c031dd9
Gopal Gupta <gopaljilab@gmail.com>
2025-08-24 09:01:29 +0530

    TextBox OOps

--- src/oops/textBox/TextBox.java
+++ src/oops/textBox/TextBox.java
@@ -1,7 +1,17 @@
 package src.oops.textBox;
 
-public class Lab1_TextBox {
-    public String text;     //attributes/ data/ field
+public class TextBox extends UIControl{
+    private String text = "";     //attributes/ data/ field
+
+    public TextBox() {
+        super(true);
+        System.out.println("TextBox");
+    }
+
+    @Override
+    public String toString(){
+        return text;
+    }
 
     public void setText(String text){
         this.text = text;
--- src/oops/textBox/UIControl.java
+++ src/oops/textBox/UIControl.java
@@ -1,4 +1,22 @@
 package src.oops.textBox;
 
-public class UIControl {
+public class UIControl{
+    private boolean isEnabled = true;
+
+    public UIControl(boolean isEnabled){
+        this.isEnabled = isEnabled;
+        System.out.println("UIControl");
+    }
+
+    public void enabled(){
+        isEnabled = true;
+    }
+
+    public void disable(){
+        isEnabled = false;
+    }
+
+    public boolean isEnabled() {
+        return isEnabled;
+    }
 }
